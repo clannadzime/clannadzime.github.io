6537640a721d957210ebf7e2909e36f162516e0e
clannadzime <2325464205@qq.com>
2024-08-03 15:27:54 +0800

    Site updated: 2024-08-03 15:27:54

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722668428839";
+const VERSION = "1722670069047";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
