1c36852b31d17728651b16d1f54491402c65d085
clannadzime <2325464205@qq.com>
2024-08-05 11:59:21 +0800

    Site updated: 2024-08-05 11:59:21

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const swPath = "/sw.js";
+const swPath = "/project/sw.js";
 const preCache = [
-  "/images/taichi.png",
-  "/images/banner.webp",
-  "/images/taichi-fill.png",
-  "/css/loader.css",
-  "/css/style.css",
-  "/js/script.js",
+  "/project/images/taichi.png",
+  "/project/images/banner.webp",
+  "/project/images/taichi-fill.png",
+  "/project/css/loader.css",
+  "/project/css/style.css",
+  "/project/js/script.js",
 ];
-const VERSION = "1722671516054";
+const VERSION = "1722830356979";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
